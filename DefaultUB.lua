72a5e0df865f83df80cefefded43378b5405fa1a
Teyasio <teyasio@comcast.net>
2019-06-18 01:30:32 -0400

    Updated TOC version

--- DefaultUB.lua
+++ DefaultUB.lua
@@ -10,7 +10,7 @@
 local MyAddon, GUB = ...
 
 GUB.DefaultUB = {}
-GUB.DefaultUB.Version = 100
+GUB.DefaultUB.Version = 101
 
 -------------------------------------------------------------------------------
 -- UnitBar table data structure.
